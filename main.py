89d5be489cffeff409e809d3f93f457d8ab2bc40
Matthew J J <98533759+mattan27@users.noreply.github.com>
2022-02-15 15:00:48 +0000

    Turtle Drawing Exercise

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
-mylist = (1.5, 3.4, 5.1, 7.7)
-print(mylist[0])
-print(mylist[1])
-print(mylist[2])
-print(mylist[3])
+"""
+This is the Template Repl for Python with Turtle.
+
+Python with Turtle lets you make graphics easily in Python.
+
+Check out the official docs here: https://docs.python.org/3/library/turtle.html
+"""
+
+import turtle
+
+t = turtle.Turtle()
+t.shape('turtle')
+sides = 6
+
+for i in range(sides):
+    t.forward(75)
+    t.left(360/sides)
+
